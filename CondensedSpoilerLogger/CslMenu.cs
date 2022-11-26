9f00c91b9f6fdcd50e403520dde29cbfd4d59955
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-11-26 12:08:15 +0000

    Improvements to CslMenu
    Change Write Spoiler Logs to Open Log Folder when button is clicked

--- CondensedSpoilerLogger/CslMenu.cs
+++ CondensedSpoilerLogger/CslMenu.cs
@@ -4,15 +4,15 @@ using MenuChanger.MenuElements;
 using Modding;
 using RandomizerMod.Logging;
 using RandomizerMod.RC;
-using System;
 using UnityEngine.UI;
 using static RandomizerMod.Localization;
+using static RandomizerMod.Menu.RandomizerMenu;
 
 namespace CondensedSpoilerLogger
 {
     public static class CslMenu
     {
-        private static SpriteManager CslSpriteManager = new(typeof(CslMenu).Assembly, "CondensedSpoilerLogger.Resources.");
+        private static readonly SpriteManager CslSpriteManager = new(typeof(CslMenu).Assembly, "CondensedSpoilerLogger.Resources.");
 
         internal static void Hook()
         {
@@ -22,19 +22,20 @@ namespace CondensedSpoilerLogger
 
         private static bool CreateButton(RandoController rc, MenuPage landingPage, out BaseButton button)
         {
-            BigButton menuButton = new(landingPage, CslSpriteManager.GetSprite("Quill"), Localize("Write Spoiler Logs Immediately"));
+            BigButton menuButton = new(landingPage, CslSpriteManager.GetSprite("Quill"), Localize("Write Spoiler Logs"));
             bool writtenLogs = false;
             menuButton.OnClick += () =>
             {
                 if (writtenLogs)
                 {
-                    CondensedSpoilerLogger.instance.LogWarn("Logs already written");
+                    OpenFile(null, string.Empty, DirectoryOptions.RecentLogFolder);
                     return;
                 }
                 
                 ReflectionHelper.CallMethod(typeof(LogManager), "WriteLogs", rc.args);
                 writtenLogs = true;
-                menuButton.Button.transform.Find("Text").GetComponent<Text>().color = Colors.FALSE_COLOR;
+                menuButton.Button.transform.Find("Text").GetComponent<Text>().text = Localize("Open Log Folder");
+                menuButton.Button.transform.Find("Image").GetComponent<Image>().sprite = CslSpriteManager.GetSprite("Map");
             };
 
             button = menuButton;
