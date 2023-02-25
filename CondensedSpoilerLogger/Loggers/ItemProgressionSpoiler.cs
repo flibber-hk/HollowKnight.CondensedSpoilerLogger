f31acd84c740d430b72394b9044e36f3f8902928
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-02-25 10:43:54 +0000

    Remove SetupPM from IPS

--- CondensedSpoilerLogger/Loggers/ItemProgressionSpoiler.cs
+++ CondensedSpoilerLogger/Loggers/ItemProgressionSpoiler.cs
@@ -149,24 +149,5 @@ namespace CondensedSpoilerLogger.Loggers
             }
             return sb.ToString();
         }
-
-        public void SetupPM(RandoModContext ctx, out LogicManager lm, out ProgressionManager pm, out MainUpdater mu)
-        {
-            // Clone LM for thread safety reasons
-            lm = new(new LogicManagerBuilder(ctx.LM));
-
-
-            pm = new(lm, ctx);
-            mu = pm.mu;
-
-            mu.AddWaypoints(lm.Waypoints);
-            mu.AddTransitions(lm.TransitionLookup.Values);
-            mu.AddPlacements(ctx.Vanilla);
-            if (ctx.transitionPlacements is not null)
-            {
-                mu.AddEntries(ctx.transitionPlacements.Select(t => new PrePlacedItemUpdateEntry(t)));
-            }
-            mu.StartUpdating();
-        }
     }
 }
