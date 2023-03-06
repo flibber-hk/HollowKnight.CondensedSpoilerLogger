63bbd0c27ded92bb1e576e3545ae6a5aa14f8f11
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-03-06 05:09:35 +0000

    Add QueryCache module

---
+++ CondensedSpoilerLogger/SpecificProgression/QueryCache.cs
@@ -0,0 +1,44 @@
+﻿using ItemChanger;
+using ItemChanger.Modules;
+using MenuChanger;
+using CacheDict = System.Collections.Generic.Dictionary<(string, CondensedSpoilerLogger.SpecificProgression.QueryType), string>;
+
+namespace CondensedSpoilerLogger.SpecificProgression
+{
+    public class QueryCache : Module
+    {
+        public override void Initialize() { }
+        public override void Unload() { }
+
+        public CacheDict Cache;
+
+        /// <summary>
+        /// Thread safe - record the given query result
+        /// </summary>
+        public static void Record(string query, QueryType queryType, string message)
+        {
+            ThreadSupport.BeginInvoke(() =>
+            {
+                if (ItemChangerMod.Modules?.GetOrAdd<QueryCache>() is QueryCache qc)
+                {
+                    qc.Cache[(query, queryType)] = message;
+                }
+            });
+        }
+
+        public static CacheDict GetCache()
+        {
+            CacheDict cache = ThreadSupport.BlockUntilInvoked<CacheDict>(() =>
+            {
+                if (ItemChangerMod.Modules?.GetOrAdd<QueryCache>() is QueryCache qc)
+                {
+                    return new(qc.Cache);
+                }
+
+                return null;
+            });
+
+            return cache;
+        }
+    }
+}
